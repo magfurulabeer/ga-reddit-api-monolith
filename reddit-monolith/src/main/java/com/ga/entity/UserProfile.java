20b53cd5337061e3bdaeca6dca78d9dc36ffe31f
magfurulabeer <magfurulabeer@gmail.com>
2019-10-22 14:26:04 -0400

    Create UserProfile entity

---
+++ reddit-monolith/src/main/java/com/ga/entity/UserProfile.java
@@ -0,0 +1,58 @@
+package com.ga.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class UserProfile {
+	@Id
+	@Column(name = "profile_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long profileId;
+	
+	@Column
+	private String additionalEmail;
+	
+	@Column
+	private String mobile;
+	
+	@Column
+	private String address;
+
+	public Long getProfileId() {
+		return profileId;
+	}
+
+	public void setProfileId(Long profileId) {
+		this.profileId = profileId;
+	}
+
+	public String getAdditionalEmail() {
+		return additionalEmail;
+	}
+
+	public void setAdditionalEmail(String additionalEmail) {
+		this.additionalEmail = additionalEmail;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
