a0320efe003cb3dec05ec5f571f76f2d47090709
magfurulabeer <magfurulabeer@gmail.com>
2019-10-22 16:08:56 -0400

    Create user profile flow

---
+++ reddit-monolith/src/main/java/com/ga/service/UserProfileService.java
@@ -0,0 +1,11 @@
+package com.ga.service;
+
+import com.ga.entity.UserProfile;
+
+public interface UserProfileService {
+	
+	public UserProfile createUserProfile(String username, UserProfile newProfile);
+	
+	public UserProfile getUserProfile(String username);
+
+}
