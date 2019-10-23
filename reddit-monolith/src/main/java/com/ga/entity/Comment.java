5c7a837f65a9c1a8ae541965d3b246097754f6fc
magfurulabeer <magfurulabeer@gmail.com>
2019-10-23 16:43:45 -0400

    Create comment entity without links

---
+++ reddit-monolith/src/main/java/com/ga/entity/Comment.java
@@ -0,0 +1,51 @@
+package com.ga.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="comments")
+public class Comment {
+/*
+ * comment_id
+ * text
+ * user_id
+ * post_id
+ */
+	
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long commentId;
+	
+	@Column(nullable = false)
+	private String text;
+
+	public Long getCommentId() {
+		return commentId;
+	}
+
+	public void setCommentId(Long commentId) {
+		this.commentId = commentId;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+	
+	
+	
+}
