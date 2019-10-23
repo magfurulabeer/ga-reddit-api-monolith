d91b194fd5e5bc436467aa958a50b513bb4255ea
John Acocella <jacocella@hotmail.com>
2019-10-22 22:00:10 -0400

    Created post entity

---
+++ reddit-monolith/src/main/java/com/ga/entity/Post.java
@@ -0,0 +1,49 @@
+package com.ga.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="posts")
+public class Post {
+	@Id
+	@Column(name="post_id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long postId;
+	
+	@Column(nullable = false)
+	private String title;
+	
+	@Column(nullable = false)
+	private String description;
+
+	public Long getPostId() {
+		return postId;
+	}
+
+	public void setPostId(Long postId) {
+		this.postId = postId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
