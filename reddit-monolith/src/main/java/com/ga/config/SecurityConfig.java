2d309b46b6b078377ae15c993ddf46c6d128eb5d
magfurulabeer <magfurulabeer@gmail.com>
2019-10-24 14:23:40 -0400

    Add cors config

--- reddit-monolith/src/main/java/com/ga/config/SecurityConfig.java
+++ reddit-monolith/src/main/java/com/ga/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.ga.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,6 +16,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -29,13 +34,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		return new BCryptPasswordEncoder();
 	}
 	
+	@Bean
+    CorsConfigurationSource corsConfigurationSource() {
+      CorsConfiguration configuration = new CorsConfiguration();
+      configuration.setAllowedOrigins(Arrays.asList("*"));
+      configuration
+          .setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
+      configuration.setAllowedHeaders(
+          Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization", "cache-control"));
+      configuration.setAllowCredentials(true);
+      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+      source.registerCorsConfiguration("/**", configuration);
+      return source;
+    }
+	
 	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService());
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-	    http.csrf().disable()
+	    http.cors().and()
+	    	.csrf().disable()
 	        .authorizeRequests()
 	        .antMatchers("/user/signup/**", "/user/login/**", "/post/list/**", "post/{id}/comment").permitAll()
 	        .antMatchers("/user/**", "/profile/**", "/post/**", "/comment/**").authenticated()
